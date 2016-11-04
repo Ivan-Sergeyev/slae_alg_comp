5839f67cd96adcced378d7185c8a693e5613fcf4
IlyaFed <ilya.d.fedorov@phystech.edu>
2016-11-05 02:51:00 +0300

    add time counting

--- model/gauss_method.h
+++ model/gauss_method.h
@@ -8,6 +8,10 @@
 
 class GaussMethod{ // : public GenericMethod {
 private:
+
+    unsigned int start_time; 
+
+    unsigned int end_time;
 	void find_max_and_swap(Matrix* M, Vector* b, int j) const {
 		int max_index = j;
 		for (int i = j+1; i < M->get_size(); i++) if ((*M)(i,j) > (*M)(max_index, j)) max_index = i;
@@ -27,7 +31,7 @@ private:
 public:
 //	GaussMethod() : GenericMethod("Gauss Method") {}
 	
-	Vector* get_answer_from_triangle(Matrix M, Vector b) const{
+	Vector* get_answer_from_triangle(Matrix M, Vector b) {
 		int _size = b.get_size();
 		Vector* result = new Vector(_size);
 		double cash;
@@ -36,13 +40,18 @@ public:
 			for (int i = j+1; i < _size; i++) cash -= M(j,i)*(*result)(i);
 			(*result)(j) = cash/M(j,j);
 		}
+
+    	end_time = clock(); 
+    	printf("gauss = %f\n", (end_time - start_time)*1.0/CLOCKS_PER_SEC);
 		return result;
 	}
 
-	Vector* run(Matrix A, Vector f) const {
+	Vector* run(Matrix A, Vector f) {
 		Matrix *M = new Matrix(A);
 		Vector *b = new Vector(f);
 		double coeff;
+
+    	start_time = clock(); 
 		int _size = b->get_size();
 		for( int j = 0; j < _size; j++) {
 			find_max_and_swap(M, b, j);
