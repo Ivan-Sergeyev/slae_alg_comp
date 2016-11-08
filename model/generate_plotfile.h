509fd34d7eda9d706fcbf09862d6fd3d47628de0
Ivan Sergeyev <i.i.sergeyev@gmail.com>
2016-11-08 21:21:32 +0300

    added 3rd power to approximation polynome

--- model/generate_plotfile.h
+++ model/generate_plotfile.h
@@ -42,10 +42,12 @@ void generate_plotfile(string plot_filename, string graph_filename,
 		plot_file <<
 			"file_" << i << " = \"" << data_relpath << "\"\n"
 			"title_" << i << " = \"" << method_name << "\"\n"
-			"a_" << i << " = 1; b_" << i << " = 1; c_" << i << " = 1\n"
-			"f_" << i << "(x) = a_" << i << "*x**2 + b_" << i << "*x + c_" << i << "\n"
+			"a3_" << i << " = 1; a2_" << i << " = 1; " <<
+			"a1_" << i << " = 1; a0_" << i << "= 1\n" <<
+			"f_" << i << "(x) = x**3*a3_" << i << " + x**2*a2_" << i << " + " <<
+			"x*a1_" << i << " + a0_" << i << "\n" <<
 			"fit f_" << i << "(x) file_" << i << " using 1:2 "
-			"via a_" << i << ", b_" << i << ", c_" << i << "\n\n";
+			"via a3_" << i << ", a2_" << i << ", a1_" << i << ", a0_" << i << "\n\n";
 	}
 
 	plot_file << "plot \\\n";
