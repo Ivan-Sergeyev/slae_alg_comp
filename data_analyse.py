23fc53177a25a7c126ee1e431fd376d29e386413
IlyaFed <ilya.d.fedorov@phystech.edu>
2016-11-14 20:21:24 +0300

    improve algoritm

--- data_analyse.py
+++ data_analyse.py
@@ -3,28 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from scipy.optimize import curve_fit
 
-names = {"gauss": "data_converged_Gauss Method.txt", "over05":"data_converged_Overrelaxation Method with tau=0.500000.txt",  "seidel": "data_converged_Seidel Method.txt", "jacobi_con":"data_converged_Jacobi Method.txt",  "over15":"data_converged_Overrelaxation Method with tau=1.500000.txt", "jacobi_div":"data_diverged_Jacobi Method.txt"}
-
-gauss = pd.read_csv("data/"+names["gauss"], header = None, sep=' ');
-
-gauss.plot.scatter(x=0, y = 1)
-plt.savefig("graph_python/gauss_n.png", dpi=300)
-
-
-seidel = pd.read_csv("data/"+names["seidel"], header = None, sep=' ');
-seidel.fillna(0,inplace=True)
-
-ax = plt.subplot();
+names = ["Gauss Method", "Overrelaxation Method with tau=0.500000",  "Seidel Method", "Jacobi Method", "Overrelaxation Method with tau=1.500000"]
+len = 5
 
 colors = {0: 'red', 2: 'black', 4: 'green', 8: 'yellow', 16: 'blue', 32: 'aqua', 64: 'c', 128: 'm', 256: 'brown', 512: 'violet', 1024: 'darkgreen'}
 
-#seidel[seidel[2]==0].plot.scatter(x=0, y = 1,label = "without mu",color=colors[0], ax=ax);
-for i in range(10):
-	n = 2**(i+1)
-	seidel[seidel[2]==n].plot.scatter(x=0, y = 1, label = "mu = "+str(n),color=colors[n], ax=ax)
 
-plt.legend(loc=2, fontsize='xx-small')
-plt.grid(True)
-plt.xlabel('size of matrix')
-plt.ylabel('T, c')
-plt.savefig("graph_python/seidel_n.png", dpi=300)
+data = dict()
+#data_collected
+for i in range(len):
+	data[i] = pd.read_csv("data/data_converged_"+names[i]+".txt", header = None, sep=' ');
+
+for i in range(len):
+	ax = plt.subplot()
+	for k in range(10):
+		n = 2**(k+1)
+		data[i][data[i][2]==n].plot.scatter(x=0, y = 1, label = "mu = "+str(n),color=colors[n], ax=ax)
+
+	plt.legend(loc=2, fontsize='xx-small')
+	plt.grid(True)
+	plt.xlabel('size of matrix')
+	plt.ylabel('T, c')
+	plt.title(names[i])
+	plt.savefig("graph_python/"+names[i]+"Time(size of matrix).png", dpi=300)
+	plt.clf()
