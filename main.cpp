55fd1d38e60eeb8507c53b1f216e091d52c1b982
Ivan Sergeyev <i.i.sergeyev@gmail.com>
2016-11-14 23:08:59 +0300

    in main.cpp: added tests for jacobi method, added log labels, added status check after calling gnuplot

--- main.cpp
+++ main.cpp
@@ -22,15 +22,17 @@ using std::system;
 #ifndef NDEBUG  // debug version
 
 #include "model/linear_algebra_test.h"
+#include "model/jacobi_method_test.h"
 
 
 void test() {
 	typedef int (*test_funcion_pointer)();
 
-	const int num_modules = 1;
-	string module_name[num_modules] = {string("linear_algebra")};
+	const int num_modules = 2;
+	string module_name[num_modules] = \
+		{string("linear_algebra"), string("jacobi_method")};
 	test_funcion_pointer module_test[num_modules] =
-		{linear_algebra_test::test};
+		{linear_algebra_test::test, jacobi_method_test::test};
 
 	int total_fails = 0;
 	for (int i = 0; i < num_modules; ++i)
@@ -67,14 +69,14 @@ void fill_geom_progr(double *sizes, int num_elems, double start, double mul) {
 int main(int argc, char **argv) {
 #ifndef NDEBUG
 	if (argc == 2 && string(argv[1]) == string("test")) {
-		cerr << "commence testing\n";
+		cerr << "[info] commence testing\n";
 		test();
 		return 0;
 	}
 #endif  // NDEBUG
 
 // setup
-	cerr << "commence setup\n";
+	cerr << "[info] commence setup\n";
 
 	// populate a list of small sizes
 	int small_num_sizes = 30,
@@ -99,7 +101,7 @@ int main(int argc, char **argv) {
 
 	if (argc == 2 && string(argv[1]) == string("test_run")) {
 		// only run on small number of tests
-		cerr << "test_run mode enabled\n";
+		cerr << "[info] test_run mode enabled\n";
 		small_num_sizes = 1;
 		large_num_sizes = 0;
 	}
@@ -124,23 +126,24 @@ int main(int argc, char **argv) {
 	for(; m_idx < num_or_methods; ++m_idx) {
 		tau += (tau < 1.0l) * 0.5l + (tau >= 1.0l) * tau_step;
 		methods[m_idx] = new OverrelaxationMethod(tau, tolerance, max_faults);
-		cerr << "added " << methods[m_idx]->get_name() << "\n";
+		cerr << "[info] added " << methods[m_idx]->get_name() << "\n";
 	}
 
 	// add jacobi method
 	JacobiMethod jacobi_method(tolerance, max_faults);
 	methods[m_idx] = &jacobi_method;
-	cerr << "added " << methods[m_idx]->get_name() << "\n";
+	cerr << "[info] added " << methods[m_idx]->get_name() << "\n";
 	++m_idx;
 	// add gauss method
 	GaussMethod gauss_method;
 	methods[m_idx] = &gauss_method;
-	cerr << "added " << methods[m_idx]->get_name() << "\n";
+	cerr << "[info] added " << methods[m_idx]->get_name() << "\n";
 	++m_idx;
 	assert(m_idx == num_methods);
 
 // perform measurements
-	cerr << "commence measurements\n";
+	cerr << "[info] commence measurements\n";
+	// todo: use format strings
 	string data_filename_format = string("./data/data_%s_%s.txt");
 
 	PerformanceComparator p_comp(cerr);
@@ -155,7 +158,7 @@ int main(int argc, char **argv) {
 						  num_runs, data_filename_format);
 
 // prepare plot and run gnuplot
-	cerr << "generate plotfiles\n";
+	cerr << "[info] commence generating plotfiles\n";
 
 	// todo: use format strings
 	string plot_all = string("./gnuplot/plot_all.plt"),
@@ -172,20 +175,30 @@ int main(int argc, char **argv) {
 					  data_format_rel, num_methods - 1, methods);
 
 // run gnuplot with plot_dir as current directory
-	cerr << "plot graphs\n";
+	cerr << "[info] commence plotting graphs\n";
+	// todo: concatenate commands
 	const char gnuplot_call_all[] = "gnuplot plot_all.plt";
 	const char gnuplot_call_num[] = "gnuplot plot_num.plt";
 
 	chdir("./gnuplot/");
-	system(gnuplot_call_all);
-	system(gnuplot_call_num);
+	int status_1 = system(gnuplot_call_all);
+	int status_2 = system(gnuplot_call_num);
 	remove("fit.log");
 	chdir("..");
 
+	if (status_1) {
+		cerr << "[error] error during system(\"" << gnuplot_call_all << "\")\n"
+			 << "        exit status " << status_1 << "\n";
+	}
+	if (status_2) {
+		cerr << "[error] error during system(\"" << gnuplot_call_num << "\")\n"
+			 << "        exit status " << status_2 << "\n";
+	}
+
 // perform cleanup
-	cerr << "commence cleanup\n";
+	cerr << "[info] commence cleanup\n";
 	delete[] methods;
 
-	cerr << "done\n";
+	cerr << "[info] done\n";
 	return 0;
 }
