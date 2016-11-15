ac5e0221ec984280b6a181ddf5a0308f2f20640a
IlyaFed <ilya.d.fedorov@phystech.edu>
2016-11-15 19:34:01 +0300

    fix some bags

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 
 TOKEN = "173564814:AAGa7wka0WesdgCGxLuszv4FHp8vNtC-e08"
 
-commands = {  # command description used in the "help" command
+commandss = {  # command description used in the "help" command
               'start': 'Вы добавлены',
               'help': 'Возможности',
               'make': 'собрать и запустить',
               'plot': 'построить графики',
-              'graph': 'получить графики (необходимо подождать после \plot)',
-              'kill': "завершить запущенную программу",
-              'pull': "получить новую версию с github",
-              'status': "получить последние 10 строк из log_err"
+              'graph': 'получить графики (необходимо подождать после plot)',
+              'kill': 'завершить запущенную программу',
+              'pull': 'получить новую версию с github',
+              'status': 'получить последние 10 строк из log_err'
 }
 
 
