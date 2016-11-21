5e899e9732430173bdd32abfc74d3c2e36832bc8
IlyaFed <ilya.d.fedorov@phystech.edu>
2016-11-21 20:50:00 +0300

    improve telegram bot place

---
+++ telegram/config.py
@@ -0,0 +1,19 @@
+﻿from telebot import types
+
+TOKEN = "173564814:AAGa7wka0WesdgCGxLuszv4FHp8vNtC-e08"
+
+commandss = {  # command description used in the "help" command
+              'start': 'Вы добавлены',
+              'help': 'Возможности',
+              'make': 'собрать и запустить',
+              'plot': 'построить графики',
+              'graph': 'получить графики (необходимо подождать после plot)',
+              'kill': 'завершить запущенную программу',
+              'pull': 'получить новую версию с github',
+              'status': 'получить последние 10 строк из log_err'
+}
+
+
+
+optionSelect = types.ReplyKeyboardMarkup(one_time_keyboard=False)  #buttons
+hideBoard = types.ReplyKeyboardHide()
